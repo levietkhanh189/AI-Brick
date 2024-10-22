6a949e65f0bb76fbc5ad35c6ba7f062bb3862983
timojourney <timo.journey@gmail.com>
2024-10-22 21:52:16 +0700

    Count Voxel - Target 650 to 900 cubes

---
+++ Assets/Scripts/CountVoxel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+public class CountVoxel : MonoBehaviour
+{
+    [Button]
+    public int Count()
+    {
+        int result = transform.childCount;
+        Debug.Log(this.gameObject.name + " has " + result + " voxels");
+        return result;
+    }
+}
