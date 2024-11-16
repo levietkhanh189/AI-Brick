173d1a517c26f53998de6c3d0eeb861406357fe0
timojourney <timo.journey@gmail.com>
2024-11-16 21:11:09 +0700

    Delete AI Toolbox

--- Assets/AI Toolbox/Runtime/Common/ErrorCodes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AiToolbox {
-public enum ErrorCodes {
-    MaxTokensExceeded = 0,
-    ThrottleExceeded = 1,
-    RemoteConfigConnectionFailure = 2,
-    RemoteConfigKeyNotFound = 3,
-    Unknown = 4,
-}
-}
